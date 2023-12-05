944dd0f86cad70ee690f176e4a88d8884d8ad20f
Daniel Horn <dkhorn@gmail.com>
2023-12-05 12:26:33 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Solution {
+
+class Solution {
+    static void Main(string[] args) {
+        var inputNumber = int.Parse(Console.ReadLine() ?? string.Empty);
+        Console.WriteLine(inputNumber.ToString());
+    }
+}
+
+}
